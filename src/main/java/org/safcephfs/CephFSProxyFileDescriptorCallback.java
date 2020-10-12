84d4a3dd4c4a73f3ecbff8117ad9db26dea7eb2a
xdavidwu <xdavidwuph@gmail.com>
2020-10-12 14:31:03 +0800

    impl openDocument

---
+++ src/main/java/org/safcephfs/CephFSProxyFileDescriptorCallback.java
@@ -0,0 +1,47 @@
+package org.safcephfs;
+
+import android.os.ProxyFileDescriptorCallback;
+import android.system.ErrnoException;
+
+import com.ceph.fs.CephMount;
+import com.ceph.fs.CephStat;
+
+public class CephFSProxyFileDescriptorCallback extends ProxyFileDescriptorCallback {
+	private CephMount cm;
+	private int fd;
+
+	public CephFSProxyFileDescriptorCallback(CephMount cm, int fd) {
+		this.cm = cm;
+		this.fd = fd;
+	}
+
+	@Override
+	public void onFsync() throws ErrnoException {
+		cm.fsync(fd, false);
+	}
+
+	@Override
+	public long onGetSize() throws ErrnoException {
+		CephStat cs = new CephStat();
+		cm.fstat(fd, cs);
+		return cs.size;
+	}
+
+	@Override
+	public int onRead(long offset, int size, byte[] data)
+		throws ErrnoException {
+		return (int) cm.read(fd, data, size, offset);
+	}
+
+	@Override
+	public void onRelease() {
+		cm.close(fd);
+		cm.unmount();
+	}
+
+	@Override
+	public int onWrite(long offset, int size, byte[] data)
+		throws ErrnoException {
+		return (int) cm.write(fd, data, size, offset);
+	}
+}
