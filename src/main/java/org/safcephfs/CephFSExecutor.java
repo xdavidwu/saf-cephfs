fe313b7352b0a2e4ccfa05d60ea2cf8dc750bb43
Pinghao Wu <xdavidwuph@gmail.com>
2025-05-04 14:13:21 +0800

    CephFSExecutor: errno: deal with api availability

--- src/main/java/org/safcephfs/CephFSExecutor.java
+++ src/main/java/org/safcephfs/CephFSExecutor.java
@@ -2,6 +2,7 @@ package org.safcephfs;
 
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.DocumentsContract;
 import android.system.ErrnoException;
@@ -10,6 +11,7 @@ import android.system.OsConstants;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.util.HashMap;
 import java.util.Map;
 
 import com.ceph.fs.CephMount;
@@ -55,91 +57,96 @@ public class CephFSExecutor {
 	 * libcephfs_jni throws IOException with message from strerror()
 	 * Bionic strerror:
 	 * https://android.googlesource.com/platform/bionic/+/refs/heads/main/libc/private/bionic_errdefs.h
-	 * sed 's|__BIONIC_ERRDEF(\([^,]*\), \([^)]*\).*|case \2 -> OsConstants.\1;|'
+	 * sed 's|__BIONIC_ERRDEF(\([^,]*\), \([^)]*\).*|strerrorT.put(\2, OsConstants.\1);|'
 	 * and delete where there are no matching OsConstants
+	 * OsConstants are not compile-time const, but initialized at runtime via initConstants jni call
 	 */
+	private static Map<String, Integer> strerrorT = new HashMap<String, Integer>();
+	static {
+		strerrorT.put("Operation not permitted", OsConstants.EPERM);
+		strerrorT.put("No such file or directory", OsConstants.ENOENT);
+		strerrorT.put("No such process", OsConstants.ESRCH);
+		strerrorT.put("Interrupted system call", OsConstants.EINTR);
+		strerrorT.put("I/O error", OsConstants.EIO);
+		strerrorT.put("No such device or address", OsConstants.ENXIO);
+		strerrorT.put("Argument list too long", OsConstants.E2BIG);
+		strerrorT.put("Exec format error", OsConstants.ENOEXEC);
+		strerrorT.put("Bad file descriptor", OsConstants.EBADF);
+		strerrorT.put("No child processes", OsConstants.ECHILD);
+		strerrorT.put("Try again", OsConstants.EAGAIN);
+		strerrorT.put("Out of memory", OsConstants.ENOMEM);
+		strerrorT.put("Permission denied", OsConstants.EACCES);
+		strerrorT.put("Bad address", OsConstants.EFAULT);
+		strerrorT.put("Device or resource busy", OsConstants.EBUSY);
+		strerrorT.put("File exists", OsConstants.EEXIST);
+		strerrorT.put("Cross-device link", OsConstants.EXDEV);
+		strerrorT.put("No such device", OsConstants.ENODEV);
+		strerrorT.put("Not a directory", OsConstants.ENOTDIR);
+		strerrorT.put("Is a directory", OsConstants.EISDIR);
+		strerrorT.put("Invalid argument", OsConstants.EINVAL);
+		strerrorT.put("File table overflow", OsConstants.ENFILE);
+		strerrorT.put("Too many open files", OsConstants.EMFILE);
+		strerrorT.put("Inappropriate ioctl for device", OsConstants.ENOTTY);
+		strerrorT.put("Text file busy", OsConstants.ETXTBSY);
+		strerrorT.put("File too large", OsConstants.EFBIG);
+		strerrorT.put("No space left on device", OsConstants.ENOSPC);
+		strerrorT.put("Illegal seek", OsConstants.ESPIPE);
+		strerrorT.put("Read-only file system", OsConstants.EROFS);
+		strerrorT.put("Too many links", OsConstants.EMLINK);
+		strerrorT.put("Broken pipe", OsConstants.EPIPE);
+		strerrorT.put("Math argument out of domain of func", OsConstants.EDOM);
+		strerrorT.put("Math result not representable", OsConstants.ERANGE);
+		strerrorT.put("Resource deadlock would occur", OsConstants.EDEADLK);
+		strerrorT.put("File name too long", OsConstants.ENAMETOOLONG);
+		strerrorT.put("No record locks available", OsConstants.ENOLCK);
+		strerrorT.put("Function not implemented", OsConstants.ENOSYS);
+		strerrorT.put("Directory not empty", OsConstants.ENOTEMPTY);
+		strerrorT.put("Too many symbolic links encountered", OsConstants.ELOOP);
+		strerrorT.put("No message of desired type", OsConstants.ENOMSG);
+		strerrorT.put("Identifier removed", OsConstants.EIDRM);
+		strerrorT.put("Device not a stream", OsConstants.ENOSTR);
+		strerrorT.put("No data available", OsConstants.ENODATA);
+		strerrorT.put("Timer expired", OsConstants.ETIME);
+		strerrorT.put("Out of streams resources", OsConstants.ENOSR);
+		if (Build.VERSION.SDK_INT >= 31) {
+			strerrorT.put("Machine is not on the network", OsConstants.ENONET);
+		}
+		strerrorT.put("Link has been severed", OsConstants.ENOLINK);
+		strerrorT.put("Protocol error", OsConstants.EPROTO);
+		strerrorT.put("Multihop attempted", OsConstants.EMULTIHOP);
+		strerrorT.put("Not a data message", OsConstants.EBADMSG);
+		strerrorT.put("Value too large for defined data type", OsConstants.EOVERFLOW);
+		strerrorT.put("Illegal byte sequence", OsConstants.EILSEQ);
+		strerrorT.put("Socket operation on non-socket", OsConstants.ENOTSOCK);
+		strerrorT.put("Destination address required", OsConstants.EDESTADDRREQ);
+		strerrorT.put("Message too long", OsConstants.EMSGSIZE);
+		strerrorT.put("Protocol wrong type for socket", OsConstants.EPROTOTYPE);
+		strerrorT.put("Protocol not available", OsConstants.ENOPROTOOPT);
+		strerrorT.put("Protocol not supported", OsConstants.EPROTONOSUPPORT);
+		strerrorT.put("Operation not supported on transport endpoint", OsConstants.EOPNOTSUPP);
+		strerrorT.put("Address family not supported by protocol", OsConstants.EAFNOSUPPORT);
+		strerrorT.put("Address already in use", OsConstants.EADDRINUSE);
+		strerrorT.put("Cannot assign requested address", OsConstants.EADDRNOTAVAIL);
+		strerrorT.put("Network is down", OsConstants.ENETDOWN);
+		strerrorT.put("Network is unreachable", OsConstants.ENETUNREACH);
+		strerrorT.put("Network dropped connection because of reset", OsConstants.ENETRESET);
+		strerrorT.put("Software caused connection abort", OsConstants.ECONNABORTED);
+		strerrorT.put("Connection reset by peer", OsConstants.ECONNRESET);
+		strerrorT.put("No buffer space available", OsConstants.ENOBUFS);
+		strerrorT.put("Transport endpoint is already connected", OsConstants.EISCONN);
+		strerrorT.put("Transport endpoint is not connected", OsConstants.ENOTCONN);
+		strerrorT.put("Connection timed out", OsConstants.ETIMEDOUT);
+		strerrorT.put("Connection refused", OsConstants.ECONNREFUSED);
+		strerrorT.put("No route to host", OsConstants.EHOSTUNREACH);
+		strerrorT.put("Operation already in progress", OsConstants.EALREADY);
+		strerrorT.put("Operation now in progress", OsConstants.EINPROGRESS);
+		strerrorT.put("Stale NFS file handle", OsConstants.ESTALE);
+		strerrorT.put("Quota exceeded", OsConstants.EDQUOT);
+		strerrorT.put("Operation Canceled", OsConstants.ECANCELED);
+	}
 	private static int cephIOEToOsConstants(IOException e) {
-		return switch (e.getMessage()) {
-		case "Operation not permitted" -> OsConstants.EPERM;
-		case "No such file or directory" -> OsConstants.ENOENT;
-		case "No such process" -> OsConstants.ESRCH;
-		case "Interrupted system call" -> OsConstants.EINTR;
-		case "I/O error" -> OsConstants.EIO;
-		case "No such device or address" -> OsConstants.ENXIO;
-		case "Argument list too long" -> OsConstants.E2BIG;
-		case "Exec format error" -> OsConstants.ENOEXEC;
-		case "Bad file descriptor" -> OsConstants.EBADF;
-		case "No child processes" -> OsConstants.ECHILD;
-		case "Try again" -> OsConstants.EAGAIN;
-		case "Out of memory" -> OsConstants.ENOMEM;
-		case "Permission denied" -> OsConstants.EACCES;
-		case "Bad address" -> OsConstants.EFAULT;
-		case "Device or resource busy" -> OsConstants.EBUSY;
-		case "File exists" -> OsConstants.EEXIST;
-		case "Cross-device link" -> OsConstants.EXDEV;
-		case "No such device" -> OsConstants.ENODEV;
-		case "Not a directory" -> OsConstants.ENOTDIR;
-		case "Is a directory" -> OsConstants.EISDIR;
-		case "Invalid argument" -> OsConstants.EINVAL;
-		case "File table overflow" -> OsConstants.ENFILE;
-		case "Too many open files" -> OsConstants.EMFILE;
-		case "Inappropriate ioctl for device" -> OsConstants.ENOTTY;
-		case "Text file busy" -> OsConstants.ETXTBSY;
-		case "File too large" -> OsConstants.EFBIG;
-		case "No space left on device" -> OsConstants.ENOSPC;
-		case "Illegal seek" -> OsConstants.ESPIPE;
-		case "Read-only file system" -> OsConstants.EROFS;
-		case "Too many links" -> OsConstants.EMLINK;
-		case "Broken pipe" -> OsConstants.EPIPE;
-		case "Math argument out of domain of func" -> OsConstants.EDOM;
-		case "Math result not representable" -> OsConstants.ERANGE;
-		case "Resource deadlock would occur" -> OsConstants.EDEADLK;
-		case "File name too long" -> OsConstants.ENAMETOOLONG;
-		case "No record locks available" -> OsConstants.ENOLCK;
-		case "Function not implemented" -> OsConstants.ENOSYS;
-		case "Directory not empty" -> OsConstants.ENOTEMPTY;
-		case "Too many symbolic links encountered" -> OsConstants.ELOOP;
-		case "No message of desired type" -> OsConstants.ENOMSG;
-		case "Identifier removed" -> OsConstants.EIDRM;
-		case "Device not a stream" -> OsConstants.ENOSTR;
-		case "No data available" -> OsConstants.ENODATA;
-		case "Timer expired" -> OsConstants.ETIME;
-		case "Out of streams resources" -> OsConstants.ENOSR;
-		case "Machine is not on the network" -> OsConstants.ENONET;
-		case "Link has been severed" -> OsConstants.ENOLINK;
-		case "Protocol error" -> OsConstants.EPROTO;
-		case "Multihop attempted" -> OsConstants.EMULTIHOP;
-		case "Not a data message" -> OsConstants.EBADMSG;
-		case "Value too large for defined data type" -> OsConstants.EOVERFLOW;
-		case "Illegal byte sequence" -> OsConstants.EILSEQ;
-		case "Socket operation on non-socket" -> OsConstants.ENOTSOCK;
-		case "Destination address required" -> OsConstants.EDESTADDRREQ;
-		case "Message too long" -> OsConstants.EMSGSIZE;
-		case "Protocol wrong type for socket" -> OsConstants.EPROTOTYPE;
-		case "Protocol not available" -> OsConstants.ENOPROTOOPT;
-		case "Protocol not supported" -> OsConstants.EPROTONOSUPPORT;
-		case "Operation not supported on transport endpoint" -> OsConstants.EOPNOTSUPP;
-		case "Address family not supported by protocol" -> OsConstants.EAFNOSUPPORT;
-		case "Address already in use" -> OsConstants.EADDRINUSE;
-		case "Cannot assign requested address" -> OsConstants.EADDRNOTAVAIL;
-		case "Network is down" -> OsConstants.ENETDOWN;
-		case "Network is unreachable" -> OsConstants.ENETUNREACH;
-		case "Network dropped connection because of reset" -> OsConstants.ENETRESET;
-		case "Software caused connection abort" -> OsConstants.ECONNABORTED;
-		case "Connection reset by peer" -> OsConstants.ECONNRESET;
-		case "No buffer space available" -> OsConstants.ENOBUFS;
-		case "Transport endpoint is already connected" -> OsConstants.EISCONN;
-		case "Transport endpoint is not connected" -> OsConstants.ENOTCONN;
-		case "Connection timed out" -> OsConstants.ETIMEDOUT;
-		case "Connection refused" -> OsConstants.ECONNREFUSED;
-		case "No route to host" -> OsConstants.EHOSTUNREACH;
-		case "Operation already in progress" -> OsConstants.EALREADY;
-		case "Operation now in progress" -> OsConstants.EINPROGRESS;
-		case "Stale NFS file handle" -> OsConstants.ESTALE;
-		case "Quota exceeded" -> OsConstants.EDQUOT;
-		case "Operation Canceled" -> OsConstants.ECANCELED;
-		default -> OsConstants.EIO;
-		};
+		var errno = strerrorT.get(e.getMessage());
+		return errno == null ? OsConstants.EIO : errno;
 	}
 
 	protected <T> T execute(Operation<T> op) throws IOException {
